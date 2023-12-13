d8790ba4bc43406c2a137730cccbb39c5a6a7321
NikolasHaaseGH <liveofnikolas@googlemail.com>
2023-12-13 20:41:20 +0100

    identifier tokens now carry identifier name as optional, literal value. Also fixed minor indexing issues that caused characters immediately after identifiers or keywords to be skipped.

--- lexer.py
+++ lexer.py
@@ -20,7 +20,8 @@ keywords = {
     "while": TokenType.WHILE
 }
 
-ezcFilePath = sys.argv[1] #TODO: handle wrong intputs
+ezcFilePath = "/Users/nikolas/Documents/WS23:24/Compilers/projekt/bluecompiler/ezctest.txt"
+#ezcFilePath = sys.argv[1] #TODO: handle wrong intputs
 
 class Lexer:
     def __init__(self, filePath):
@@ -30,21 +31,23 @@ class Lexer:
         self.forward = 0
         self.currentLine = ""
 
-    def run(self):
+    def generateTokens(self):
         file = open(self.filePath, 'r')
         while True:
             self.currentLine = file.readline()
             if self.currentLine == "": break
             self.scanTokens()
+            self.reset()
         
         self.tokenList.append([TokenType.EOF.name, None])
         file.close()
 
     def reachedEnd(self):
-        return self.forward == len(self.currentLine) - 1
+        return self.forward == len(self.currentLine)
 
     def advance(self):
-        if not self.reachedEnd(): self.forward = self.forward + 1
+        if not self.reachedEnd(): 
+            self.forward = self.forward + 1
 
     def reset(self):
         self.lexemeBegin = 0
@@ -83,8 +86,8 @@ class Lexer:
         cond2 = self.isDigit(char)
         return cond1 or cond2
     
-    def isMultiOperator(self):
-        if self.lookahead() == '=':
+    def isFollowedBy(self, secondCharacter):
+        if self.lookahead() == secondCharacter:
             self.advance()
             return True
         else:
@@ -125,19 +128,18 @@ class Lexer:
         self.consumeToken(TokenType.NUMBER, float(self.currentLine[self.lexemeBegin:self.forward+1]))
 
     def handleIdentifier(self):
-        while self.isAlphaNumeric(self.peek()): 
+        while self.isAlphaNumeric(self.lookahead()): 
             self.advance()
 
-        identifier = self.currentLine[self.lexemeBegin:self.forward]
+        identifier = self.currentLine[self.lexemeBegin:self.forward+1]
 
         keyword = keywords.get(identifier) #test if identifier matches any keyword
         if keyword: 
             self.consumeToken(keyword)
         else:
-            self.consumeToken(TokenType.IDENTIFIER)
+            self.consumeToken(TokenType.IDENTIFIER, identifier)
 
     def scanTokens(self):
-        self.reset()
         while not self.reachedEnd():
             match self.peek():
                 case '(': self.consumeToken(TokenType.LEFT_PAREN)
@@ -150,10 +152,10 @@ class Lexer:
                 case '+': self.consumeToken(TokenType.PLUS)
                 case ';': self.consumeToken(TokenType.SEMICOLON)
                 case '*': self.consumeToken(TokenType.STAR)
-                case '!': self.consumeToken(TokenType.BANG_EQUAL) if self.isMultiOperator() else self.consumeToken(TokenType.BANG)
-                case '=': self.consumeToken(TokenType.EQUAL_EQUAL) if self.isMultiOperator() else self.consumeToken(TokenType.EQUAL)
-                case '<': self.consumeToken(TokenType.LESS_EQUAL) if self.isMultiOperator() else self.consumeToken(TokenType.LESS)
-                case '>': self.consumeToken(TokenType.GREATER_EQUAL) if self.isMultiOperator() else self.consumeToken(TokenType.GREATER)
+                case '!': self.consumeToken(TokenType.BANG_EQUAL) if self.isFollowedBy('=') else self.consumeToken(TokenType.BANG)
+                case '=': self.consumeToken(TokenType.EQUAL_EQUAL) if self.isFollowedBy('=') else self.consumeToken(TokenType.EQUAL)
+                case '<': self.consumeToken(TokenType.LESS_EQUAL) if self.isFollowedBy('=') else self.consumeToken(TokenType.LESS)
+                case '>': self.consumeToken(TokenType.GREATER_EQUAL) if self.isFollowedBy('=') else self.consumeToken(TokenType.GREATER)
                 case '/': self.consumeToken(TokenType.SLASH) if self.lookahead() != '/' else self.skipComment()
                 case " ": self.moveToNextLexeme()
                 case '\r': self.moveToNextLexeme()
@@ -170,5 +172,5 @@ class Lexer:
                         self.moveToNextLexeme()
 
 lexer = Lexer(ezcFilePath)
-lexer.run()
+lexer.generateTokens()
 print(lexer.tokenList)
