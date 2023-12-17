506687d31e566ed618669420687bbddd394294bb
NikolasHaaseGH <liveofnikolas@googlemail.com>
2023-12-17 14:10:16 +0100

    fixed issues with previous pointer of symboltablestack

--- symboltableStack.py
+++ symboltableStack.py
@@ -1,32 +1,31 @@
-class Symboltable:
-    def __init__(self, parent):
+class SymboltableStackItem:
+    def __init__(self, parent, previous):
         self.table = {}
         self.parent = parent # Symbol table of outer scope
+        self.previous = previous
 
     def add(self, symbol, value):
         self.table[symbol] = value
 
 class SymboltableStack:
     def __init__(self):
-        self.globalTable = Symboltable(None)
+        self.globalTable = SymboltableStackItem(None, None)
+        self.globalTable
         self.head = self.globalTable
-        self.prev = None # previous stack element, NOT necessarily parent of symbol table
 
     def pushNewTableOnCurrentContext(self):
-        newSymbolTable = Symboltable(parent= self.head)
-        self.prev = self.head   
+        newSymbolTable = SymboltableStackItem(parent= self.head, previous= self.head)
         self.head = newSymbolTable
 
     def pushNewTableOnGolbalContext(self):
-        newSymbolTable = Symboltable(parent= self.globalTable)
-        self.prev = self.head
+        newSymbolTable = SymboltableStackItem(parent= self.globalTable, previous= self.head)
         self.head = newSymbolTable
 
     def addSymbolToCurrentContext(self, symbol, value):
         self.head.add(symbol, value)
 
     def popHead(self):
-        self.head = self.prev
+        self.head = self.head.previous
         #old 'self.head' is garbage-collected
 
     def get(self, key):
@@ -39,4 +38,3 @@ class SymboltableStack:
                 chainElement = chainElement.parent
 
         return chainElement.table[key] # None if id not contained in any symbol table
-    
